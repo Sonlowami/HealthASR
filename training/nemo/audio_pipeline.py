0c50edb0b5917ef8c288177c0cf0ed534c6ceaea
Sonlowami <uwilowami64@outlook.com>
2026-07-10 16:33:54 +0200

    Fix typo

--- training/nemo/audio_pipeline.py
+++ training/nemo/audio_pipeline.py
@@ -49,7 +49,7 @@ class AudioNemoTrainer:
     
 
     def evaluate(self):
-        model = self.setup_modelw()
+        model = self.setup_model()
         results = self.trainer.validate(model)
         return results[0] if results else {}
     
