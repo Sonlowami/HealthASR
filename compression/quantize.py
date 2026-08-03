759d649b098f31b7e90e07643322f7247693d01a
Sonlowami <uwilowami64@outlook.com>
2026-08-03 12:39:11 +0200

    workaround for deepcopy error

--- compression/quantize.py
+++ compression/quantize.py
@@ -12,7 +12,24 @@ def build_calibration_dataset(model) -> nncf.Dataset:
     return nncf.Dataset(model._validation_dl, transform_fn)
 
 def quantize_model(model):
-    model.eval()
-    calibration_dataset = build_calibration_dataset(model)
-    quantized_model = nncf.quantize(model, calibration_dataset)
+    """
+    nncf.quantize()'s internal deepcopy() can't handle a wrapt.FunctionWrapper
+    stored as an instance attribute (model.training_step here appears to be
+    monkeypatched with one, likely by decorator-based instrumentation. I couldn't find where this happened).
+    Strip any such instance-level overrides
+    before quantizing, restore them after.
+    """
+    instance_overrides = {}
+    for attr in ("training_step", "trainer", "log"):
+        if attr in model.__dict__:
+            instance_overrides[attr] = model.__dict__.pop(attr)
+
+    try:
+        model.eval()
+        calibration_dataset = build_calibration_dataset(model)
+        quantized_model = nncf.quantize(model, calibration_dataset)
+    finally:
+        for attr, value in instance_overrides.items():
+            model.__dict__[attr] = value
+
     return quantized_model
